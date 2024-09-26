c73c35e869a32f885988b940372be751dd9194ef
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-26 09:10:33 -0300

    fix: tests location

--- src/test/kotlin/com/example/permission/PermissionApplicationTests.kt
+++ src/test/kotlin/com/example/permission/PermissionApplicationTests.kt
@@ -1,9 +1,7 @@
 package com.example.permission
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(classes = [com.example.permission.app.PermissionApplication::class])
 class PermissionApplicationTests {
 
     @Test
