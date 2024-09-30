f26608e468e12b82e31af3699afd568ff98fb64b
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-30 09:21:44 -0300

    feat: add TestConfig for in-memory db

--- src/test/kotlin/com/ingsis/permission/user/UserControllerTest.kt
+++ src/test/kotlin/com/ingsis/permission/user/UserControllerTest.kt
@@ -1,19 +1,23 @@
 package com.ingsis.permission.user
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.ingsis.permission.Permission
+import com.ingsis.permission.TestConfig
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-@SpringBootTest
+@SpringBootTest(classes = [Permission::class])
 @AutoConfigureMockMvc
+@Import(TestConfig::class)
 class UserControllerTest {
 
     @Autowired
