2e9a10429b2f7b94c4a2cba5e567122a44c93f77
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-30 09:39:04 -0300

    feat: add test properties file

--- src/main/kotlin/com/ingsis/permission/user/UserRepository.kt
+++ src/main/kotlin/com/ingsis/permission/user/UserRepository.kt
@@ -4,7 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByUsername(username: String): User?
-    fun findByEmail(email: String): User?
-}
+interface UserRepository : JpaRepository<User, Long>
--- src/test/kotlin/com/ingsis/permission/user/UserControllerTest.kt
+++ src/test/kotlin/com/ingsis/permission/user/UserControllerTest.kt
@@ -1,23 +1,19 @@
 package com.ingsis.permission.user
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.ingsis.permission.Permission
-import com.ingsis.permission.TestConfig
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-@SpringBootTest(classes = [Permission::class])
+@SpringBootTest(properties = ["spring.datasource.url=jdbc:h2:mem:testdb"])
 @AutoConfigureMockMvc
-@Import(TestConfig::class)
 class UserControllerTest {
 
     @Autowired
