1b2852de574c6d6ccafbda9047d09a1e8ba1254b
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-30 11:33:36 -0300

    tests: test for User endpoints

--- src/test/kotlin/com/ingsis/permission/user/UserControllerTest.kt
+++ src/test/kotlin/com/ingsis/permission/user/UserControllerTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -18,6 +19,8 @@ class UserControllerTest {
 
     @Autowired
     private lateinit var userRepository: UserRepository
+
+    private var createdUserId: Long? = null
 
     @BeforeEach
     fun setup() {
@@ -36,5 +39,52 @@ class UserControllerTest {
         assert(createdUser!!.id > 0)
         assert(createdUser.username == "john_doe")
         assert(createdUser.email == "john@example.com")
+        createdUserId = createdUser.id
+    }
+
+    @Test
+    fun `test modifyUser endpoint - success`() {
+        val userDto = UserDto("john_doe", "john@example.com", "password123")
+        val createdResponse: ResponseEntity<User> = userController.createUser(userDto)
+        val createdUser = createdResponse.body
+        assert(createdUser != null)
+        if (createdUser != null) {
+            createdUserId = createdUser.id
+        }
+
+        val updatedUserDto = UserDto("john_updated", "john_updated@example.com", "newpassword")
+        val updateResponse: ResponseEntity<User> = userController.modifyUser(createdUserId!!, updatedUserDto)
+
+        assert(updateResponse.statusCode == HttpStatus.OK)
+        val updatedUser = updateResponse.body
+
+        if (updatedUser != null) {
+            assert(updatedUser.username == "john_updated")
+            assert(updatedUser.email == "john_updated@example.com")
+        }
+    }
+
+    @Test
+    fun `test deleteUser endpoint - success`() {
+        val userDto = UserDto("john_doe", "john@example.com", "password123")
+        val createdResponse: ResponseEntity<User> = userController.createUser(userDto)
+        val createdUser = createdResponse.body
+        assert(createdUser != null)
+
+        if (createdUser != null) {
+            createdUserId = createdUser.id
+        }
+
+        val deleteResponse: ResponseEntity<Void> = userController.deleteUser(createdUserId!!)
+
+        assert(deleteResponse.statusCode == HttpStatus.NO_CONTENT)
+        assert(!userRepository.existsById(createdUserId!!))
+    }
+
+    @Test
+    fun `test deleteUser endpoint - user not found`() {
+        val response: ResponseEntity<Void> = userController.deleteUser(99999)
+
+        assert(response.statusCode == HttpStatus.NOT_FOUND)
     }
 }
