1d2433eb340a34b8de7eb70dc2a1d7a37f5b4ac8
husseymarcos <mhussey@mail.austral.edu.ar>
2024-10-02 17:23:57 -0300

    test: UserController

--- src/main/kotlin/com/ingsis/permission/user/UserRepository.kt
+++ src/main/kotlin/com/ingsis/permission/user/UserRepository.kt
@@ -1,7 +1,7 @@
 package com.ingsis.permission.user
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : CrudRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long>
