f2032f179d0c5e385a676edf927c7cf7ccc4647d
husseymarcos <mhussey@mail.austral.edu.ar>
2024-10-28 17:02:18 -0300

    chore: small fixes

--- src/main/kotlin/com/ingsis/permission/health/HealthController.kt
+++ src/main/kotlin/com/ingsis/permission/health/HealthController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HomeController {
+class HealthController {
 
   @GetMapping("/info")
   fun index(): String {
