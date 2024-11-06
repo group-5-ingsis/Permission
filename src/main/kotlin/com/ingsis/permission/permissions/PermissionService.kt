1807cf72faab0fb0bbb0d73c6f191406c1fb6677
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-06 09:54:56 -0300

    fix: first try at new User impl

--- src/main/kotlin/com/ingsis/permission/permissions/PermissionService.kt
+++ src/main/kotlin/com/ingsis/permission/permissions/PermissionService.kt
@@ -9,12 +9,31 @@ import org.springframework.stereotype.Service
 class PermissionService(@Autowired private val userRepository: UserRepository) {
 
   fun getSnippets(userId: String, type: String): List<String> {
-    val user = getUser(userId)
+    val user = checkUserExists(userId)
     return when (type) {
       "Read" -> user?.readableSnippets ?: emptyList()
       "Write" -> user?.writableSnippets ?: emptyList()
       else -> emptyList()
     }
+  }
+
+  private fun checkUserExists(userId: String): SnippetUser? {
+    val user = userRepository.findByAuth0id(userId)
+    if (user == null) {
+      val newUser = SnippetUser(
+        auth0id = userId,
+        username = getAuth0Username(),
+        readableSnippets = emptyList(),
+        writableSnippets = emptyList()
+      )
+      return userRepository.save(newUser)
+    }
+    return user
+  }
+
+  // Agarrar el username con /tokenInfo de auth0
+  private fun getAuth0Username(): String {
+    return "hello"
   }
 
   fun updatePermission(userId: String, snippetId: String, type: String): SnippetUser {
