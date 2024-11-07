97d8311a8f1dc357c530dfa76f53885142036077
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-07 12:23:54 -0300

    fix local and prod

--- src/main/kotlin/com/ingsis/permission/Permission.kt
+++ src/main/kotlin/com/ingsis/permission/Permission.kt
@@ -10,15 +10,20 @@ open class Permission
 fun main(args: Array<String>) {
   val dotenv = Dotenv.load()
 
-  System.setProperty("AUTH_SERVER_URI", dotenv["AUTH_SERVER_URI"])
-  System.setProperty("AUTH_CLIENT_ID", dotenv["AUTH_CLIENT_ID"])
-  System.setProperty("AUTH_CLIENT_SECRET", dotenv["AUTH_CLIENT_SECRET"])
-  System.setProperty("AUTH0_AUDIENCE", dotenv["AUTH0_AUDIENCE"])
-  System.setProperty("SERVER_PORT", dotenv["SERVER_PORT"])
-  System.setProperty("SPRING_DATASOURCE_URL", dotenv["SPRING_LOCAL_DATASOURCE_URL"])
-  System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv["SPRING_DATASOURCE_USERNAME"])
-  System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv["SPRING_DATASOURCE_PASSWORD"])
-  System.setProperty("CLAIMS_KEY", dotenv["CLAIMS_KEY"])
+  val profile = dotenv["SPRING_PROFILES_ACTIVE"] ?: "local"
+  System.setProperty("spring.profiles.active", profile)
+
+  if (profile == "local") {
+    System.setProperty("AUTH_SERVER_URI", dotenv["AUTH_SERVER_URI"])
+    System.setProperty("AUTH_CLIENT_ID", dotenv["AUTH_CLIENT_ID"])
+    System.setProperty("AUTH_CLIENT_SECRET", dotenv["AUTH_CLIENT_SECRET"])
+    System.setProperty("AUTH0_AUDIENCE", dotenv["AUTH0_AUDIENCE"])
+    System.setProperty("SERVER_PORT", dotenv["SERVER_PORT"])
+    System.setProperty("SPRING_LOCAL_DATASOURCE_URL", dotenv["SPRING_LOCAL_DATASOURCE_URL"])
+    System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv["SPRING_DATASOURCE_USERNAME"])
+    System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv["SPRING_DATASOURCE_PASSWORD"])
+    System.setProperty("CLAIMS_KEY", dotenv["CLAIMS_KEY"])
+  }
 
   runApplication<Permission>(*args)
 }
