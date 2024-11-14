5add3a2104d086c9de530a4a619ea6f743281b4b
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-11-14 17:01:52 -0300

    Al final si EnableAsync

--- src/main/kotlin/com/ingsis/permission/Permission.kt
+++ src/main/kotlin/com/ingsis/permission/Permission.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
 open class Permission
 
 fun main(args: Array<String>) {
